69e6f23e499fc4e63aef1623c8cbcaa806985dbf
JerecoPiso <jerecopiso@github.com>
2023-04-12 17:22:28 +0800

    add service

--- src/App.js
+++ src/App.js
@@ -56,22 +56,30 @@ function App() {
                   <div style={{ textAlign: 'center' }}>
                       <FontAwesomeIcon icon={solidIcons.faLaptop} size="4x" color='#394867' />
                       <p>Web Application</p>
+                      <p className='service-desc'>Empowering businesses and individuals to thrive in the digital age through innovative web solutions. Crafting custom websites that elevate your brand and engage your audience. Experience the power of a strong online presence with my expert web development services.</p>
                   </div>
               </div>
               <div className='services-type'> 
                   <div style={{ textAlign: 'center' }}>
                       <FontAwesomeIcon icon={brandIcons.faAndroid} size="4x" color='#394867' />
                       <p>Android Application</p>
+                      <p className='service-desc'>
+                      Unlocking the potential of Android technology to build powerful mobile solutions for modern-day challenges. Designing intuitive interfaces and robust functionalities that enhance user engagement and satisfaction. With my Android development expertise, your mobile app dreams can become a reality.
+                      </p>
                   </div>
               </div>
               <div className='services-type'>
                   <div style={{ textAlign: 'center' }}>
                       <FontAwesomeIcon icon={solidIcons.faDesktop} size="4x" color='#394867' />
                       <p>Desktop Application</p>
+                      <p className='service-desc'>
+                      Revolutionize the way you work with our powerful, user-friendly desktop applications designed to boost productivity. Using the latest technologies to create customized solutions that meet your unique needs. From ideation to launch, we provide end-to-end desktop development services to bring your vision to life.
+                      </p>
                   </div>
               </div>
             </div>
         </div>
+    
     </div>
     // end class app
   );
